187fddc83877b2d83d509472fd96300c230b5650
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-04-02 11:36:24 +0530

    Update __main__.py
    Signed-off-by: allentertainmentsstudio <allentertainmentsstudio@gmail.com>

--- anony/__main__.py
+++ anony/__main__.py
@@ -9,7 +9,7 @@ from anony import app, db, logger
 from anony.modules import all_modules
 
 
-# 🔥 Flask Web Server (Deploy ke liye)
+# Flask app
 web_app = Flask('')
 
 @web_app.route('/')
@@ -17,17 +17,18 @@ def home():
     return "Bot is running!"
 
 def run_web():
-    PORT = int(os.environ.get("PORT", 3000))
+    PORT = int(os.environ.get("PORT", 5000))
     web_app.run(host='0.0.0.0', port=PORT)
-
-Thread(target=run_web).start()
 
 
 async def anony_boot():
     try:
         await app._start()
+        print("Bot Started ✅")
     except Exception as ex:
-        raise RuntimeError(ex)
+        print("Bot Error ❌:", ex)
+        return
+
     await db.connect()
 
     for module in all_modules:
@@ -40,5 +41,8 @@ async def anony_boot():
 
 
 if __name__ == "__main__":
+    # 🔥 Flask ko yaha start karo
+    Thread(target=run_web).start()
+
     asyncio.get_event_loop().run_until_complete(anony_boot())
     logger.info("Stopping String Gen Bot...")
